4cd20b6d9ddcc9fc20c170e19fa0c7404c44f9aa
Sergey Voloshin <git@varme.pw>
2021-12-13 00:11:47 +0300

    base config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,118 +1,88 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const darkCodeTheme = require('prism-react-renderer/themes/oceanicNext');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    title:                 'Deploy Local',
+    tagline:               'Site deployment assistant locally',
+    url:                   'https://dl.varme.pw',
+    baseUrl:               '/',
+    onBrokenLinks:         'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon:               'img/favicon.ico',
+    organizationName:      'local-deploy',
+    projectName:           'dl',
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs:  {
+                    sidebarPath:   require.resolve('./sidebars.js'),
+                    routeBasePath: '/',
+                    editUrl:       'https://github.com/varrcan/notes/edit/master/',
+                    admonitions:   {},
+
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            navbar: {
+                style: 'dark',
+                title: 'Deploy Local',
+                hideOnScroll: true,
+                logo:  {
+                    alt: 'dl',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type:     'doc',
+                        docId:    'intro',
+                        position: 'left',
+                        label:    'Docs',
+                    },
+                    {to: '/blog', label: 'Blog', position: 'left'},
+                    {
+                        href:     'https://github.com/local-deploy/dl',
+                        label:    'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style:     'dark',
+                // links:     [
+                //     {
+                //         title: '',
+                //         items: [
+                //             {
+                //                 label: '',
+                //                 to:    '',
+                //             },
+                //         ],
+                //     }
+                // ],
+                copyright: `Copyright © ${new Date().getFullYear()} Varrcan`,
+            },
+            prism:  {
+                theme:     lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                additionalLanguages: ['apacheconf', 'docker', 'editorconfig', 'php', 'nginx', 'regex'],
+            },
+             colorMode: {
+                 defaultMode: 'dark',
+             },
+        }),
 };
 
 module.exports = config;
